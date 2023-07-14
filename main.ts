8629bed7046d588a01062857cfe5492b7f1ff617
ViddyCoder <vu.dang00@gmail.com>
2023-07-14 01:23:48 -0500

    feat: Select words, paragraphs, and more with one click.

--- main.ts
+++ main.ts
@@ -1,137 +1,213 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Console } from 'console';
+import { Editor, EditorPosition, MarkdownView, Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+enum Granularity {
+	Word = "WORD",
+	Clause = "CLAUSE",
+	Sentence = "SENTENCE",
+	Paragraph = "PARA",
+	Section = "SECTION"
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const GraduatedSelectionDict = {
+	[Granularity.Word] : Granularity.Clause,
+	[Granularity.Clause] : Granularity.Sentence,
+	[Granularity.Sentence] : Granularity.Paragraph,
+	[Granularity.Paragraph] : Granularity.Section,
+	[Granularity.Section] : Granularity.Section
+};
+
+let gNextGranularity = Granularity.Word;
+
+const SECTION_PATTERN = /((.*[\r\n])*)#+\s/gm;
+const PARA_PATTERN = /([^\r\n]+[\r\n])/gm;
+const SENTENCE_PATTERN = /([^\r\n.?!]+(\.[0-9]+)*[.?!"]*[^\S\r\n]*)/gm;
+const CLAUSE_PATTERN = /([^\r\n\"\{\}\[\].?!,:;]+(\.[0-9]+)*[.?!,:;]*[^\S\r\n]*)[\r\n]*/gm;
+const WORD_PATTERN = /(([\*~=]+)*[a-zA-Z0-9'~@#$-]+(\.[0-9]+)*([\*~=]+)*)/gm;
+
+const GranularityToPattern = {
+	[Granularity.Word] : WORD_PATTERN,
+	[Granularity.Clause] : CLAUSE_PATTERN,
+	[Granularity.Sentence] : SENTENCE_PATTERN,
+	[Granularity.Paragraph] : PARA_PATTERN,
+	[Granularity.Section] : SECTION_PATTERN
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+const WORD_SEARCH_RADIUS = 500; // 1000 Characters
 
-	async onload() {
-		await this.loadSettings();
+function smartSelect(editor: Editor) {
+	const cursorPosA = editor.getCursor("anchor");
+	const cursorPosB = editor.getCursor("head");
+	const lineText = editor.getLine(cursorPosA.line);
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+	if (lineText.trimEnd().length === 0) {
+		return;
+	}
+	
+	console.debug("Processing current granularity: " + gNextGranularity.valueOf());
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+	if(cursorChanged(editor)) {
+		gNextGranularity = Granularity.Word;
+		console.debug("granularity reset to word");
+	}
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+	if (gNextGranularity === Granularity.Section) {
+		smartSelectSection(editor);
+		return;
+	}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+	if (gNextGranularity === Granularity.Paragraph) {
+		if (cursorPosA.ch === 0 && cursorPosB.ch === lineText.length) {
+			console.debug("All we did was select the already selected paragraph. Go to next granularity.");
+			gNextGranularity = GraduatedSelectionDict[gNextGranularity];
+			savePosition(editor);
+			smartSelect(editor);
+		} else {
+			editor.setSelection({ line: cursorPosA.line, ch: 0 }, { line: cursorPosA.line, ch: lineText.length });
+			savePosition(editor);
+			gNextGranularity = GraduatedSelectionDict[gNextGranularity];
+		}
+		return;
+	}
+
+	let match;
+	let matchPositions = [];
+	let pattern = GranularityToPattern[gNextGranularity];
+
+	while((match = pattern.exec(lineText)) !== null) {
+		
+		let index = match.index;
+		let length = match[0].length;
+		if (length === 0) { continue; }
+
+		if (gNextGranularity === Granularity.Clause) {
+			for (let i = 0; i < match[0].length; i++) {
+				if (match[0].substring(i, 1) === ' ') {
+					index++;
+					length--;
+				}
+				else {
+					break;
 				}
 			}
+		}
+		matchPositions.push([index, length]);
+	}
+
+	let foundSelection = false;
+	for (var i = matchPositions.length - 1; i >= 0; i--) {
+		let posA = matchPositions[i][0];
+		let posB = posA + matchPositions[i][1];
+
+		if (cursorPosA.ch < posA) {
+			continue;
+		}
+
+		if (gNextGranularity === Granularity.Word && cursorPosB.ch > posB) {
+			continue
+		} 
+
+		if (cursorPosA.ch === posA && cursorPosB.ch === posB) {
+			gNextGranularity = GraduatedSelectionDict[gNextGranularity];
+			console.debug("New selection equals old. Go up one granularity.");
+			smartSelect(editor);
+			break;
+		}
+
+		foundSelection = true;
+		console.debug("A selection was found!");
+		editor.setSelection({line: cursorPosA.line, ch: posA}, {line: cursorPosA.line, ch: posB});
+		break;		
+	}
+
+	if (!foundSelection) {
+		console.debug("A selection was not found. Rerun at the next granularity.");
+		gNextGranularity = GraduatedSelectionDict[gNextGranularity];
+		smartSelect(editor);
+		return;
+	}
+
+	savePosition(editor);
+	gNextGranularity = GraduatedSelectionDict[gNextGranularity];
+	console.debug("Setting next granularity to: " + gNextGranularity.valueOf());
+}
+
+let lastHead: EditorPosition;
+let lastAnchor: EditorPosition;
+
+function cursorsAreEqual(a: EditorPosition, b: EditorPosition) {
+	if (!a && !b) {return true;}
+	if (!a || !b) {return false;}
+	return (a.line === b.line && a.ch === b.ch);
+}
+function cursorChanged(editor: Editor) {
+	if (cursorsAreEqual(lastHead, editor.getCursor("head"))
+		&& cursorsAreEqual(lastAnchor, editor.getCursor("anchor"))) {
+		console.debug("cursor didn't change");
+		return false;
+	}
+	else {
+		lastHead = editor.getCursor("head");
+		lastAnchor = editor.getCursor("anchor");
+		console.debug("cursor changed");
+		return true;
+	}
+}
+function savePosition(editor: Editor) {
+	lastHead = editor.getCursor("head");
+	lastAnchor = editor.getCursor("anchor");
+}
+
+const HEADER_PATTERN = /^(#+)[^\S\r\n]+.*/gm;
+function smartSelectSection(editor: Editor) {
+	const cursorPosA = editor.getCursor("anchor");
+	const cursorPosB = editor.getCursor("head");
+	const lineText = editor.getLine(cursorPosA.line);
+
+	const lineA = cursorPosA.line;
+	const match = HEADER_PATTERN.exec(editor.getLine(lineA));
+	if (!match) {
+		return;
+	}
+
+	let headerLevelA = match[1].length;
+	let lineB = lineA + 1;
+	let matchB;
+	const lineCount = editor.lineCount();
+
+	while(lineB < lineCount) {
+		matchB = HEADER_PATTERN.exec(editor.getLine(lineB));
+		if(!matchB) {
+			lineB++; 
+			continue;
+		}
+		let headerLevelB = matchB[1].length;
+		if (headerLevelA < headerLevelB) {
+			lineB++;
+			continue
+		}
+		break;
+	}
+	editor.setSelection({line: lineA, ch: 0}, {line: lineB, ch: 0});
+	savePosition(editor);
+}
+
+export default class SelectCurrentLinePlugin extends Plugin {
+
+	async onload() {
+	
+		// This adds an editor command on the current editor instance
+		this.addCommand({
+			id: 'smart-select',
+			name: 'Smart Select',
+			icon: 'move-horizontal',
+			hotkeys: [{modifiers: [], key: 'Escape'}],
+			editorCallback: (editor: Editor) => {
+				smartSelect(editor);
+			}
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
